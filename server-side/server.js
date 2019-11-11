7e76fabe2bdf368db859394b82dabfa97ddc2d15
Mehmet Can <bmcan33@gmail.com>
2019-11-12 01:25:48 +0300

    database functions on server-side

--- server-side/server.js
+++ server-side/server.js
@@ -4,6 +4,11 @@ const bodyParser = require('body-parser');
 const PORT = 4000;
 const cors = require('cors');
 
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://admino:maraton1@cluster0-shard-00-00-jlznx.mongodb.net:27017,cluster0-shard-00-01-jlznx.mongodb.net:27017,cluster0-shard-00-02-jlznx.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin';
+
+
+
 app.use(bodyParser.json());
 
 app.listen(PORT, function(){
@@ -12,8 +17,38 @@ app.listen(PORT, function(){
 
 app.use(cors());
 
+
+app.get('/index', function(req,res){
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo.collection("cryptocurrencies").find({}).toArray(function(err, result) {
+    if (err) throw err;
+    res.json(result);
+    db.close();
+  });
+});
+
+
+});
+
+
+
  app.post('/add', function(req,res){
      res.json(req.body);
      console.log(req.body)
-   });
+   
 
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("mydb");
+  dbo.collection("cryptocurrencies").insertOne(req.body, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
+});
+
+
+   });
--- src/App.js
+++ src/App.js
@@ -1,48 +1,5 @@
-// import React from 'react';
-// import axios from 'axios';
-// import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import './App.css';
 
-
-// export default class Create extends Component {
-
-//  onSubmit(e) {
-//     e.preventDefault();
-//     var obj = {
-//       c_name: this.state.cryptocurrency_code,
-//       c_code: this.state.cryptocurrency_code
-//     };
-//     axios.post('http://localhost:4000/add', obj).then(res => console.log(res.data));
-    
-//     this.setState({
-//       cryptocurrency_name: '',
-//       cryptocurrency_code: ''
-//     })
-//   }
-
-// function App() {
-//   return (
-//  <div className="justify-content-center col-auto  row  text-center" style={{marginTop: 10}}>
-//                 <h3 className="display-4 col-md-12">Add New Business</h3>
-//                 <form onSubmit={this.onSubmit}>
-//                     <div className="form-group">
-//                         <label>Cryptocurrency Name </label>
-//                         <input type="text" value={this.state.cryptocurrency_name} className="form-control"/>
-//                     </div>
-//                     <div className="form-group">
-//                         <label>Cryptocurrency Code</label>
-//                         <input type="text" value={this.state.cryptocurrency_code} className="form-control"/>
-//                     </div>
-//                     <div className="form-group">
-//                         <input type="submit" value="Register Cryptocurrency" className="btn btn-primary"/>
-//                     </div>
-//                 </form>
-//             </div>
-//   );
-// }
-
-// export default App;
 import React, { Component } from 'react';
 import axios from 'axios';
 
@@ -54,6 +11,11 @@ super(props);
     this.onChangeCode = this.onChangeCode.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
 
+          axios.get('http://localhost:4000/index')
+        .then(res => console.log(res.data) );
+
+
+//    this.values =coins
 
     this.state = {
       cryptocurrency_name: '',
@@ -106,6 +68,29 @@ super(props);
                         <input type="submit" value="Register Cryptocurrency" className="btn btn-primary"/>
                     </div>
                 </form>
+
+            <table class="table">
+  <thead> 
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Value</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Test</td>
+      <td>Test</td>
+      </tr>
+    <tr>
+      <td>Bitcoin</td>
+      <td>1000+</td>
+      </tr>
+    <tr>
+      <td>Litecoin</td>
+      <td>100+</td>
+      </tr>
+  </tbody>
+</table>
             </div>
     )
   }
